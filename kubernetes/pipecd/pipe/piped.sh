f5f2fc9c4347a6a963df86cf2a1482121eccdd1f
yamato0211 <kumayama0211@gmail.com>
2024-06-13 20:32:58 +0900

    delete: event.yaml

--- kubernetes/pipecd/pipe/piped.sh
+++ kubernetes/pipecd/pipe/piped.sh
@@ -1,4 +1,3 @@
 helm upgrade -i piped oci://ghcr.io/pipe-cd/chart/piped --version=v0.47.2 --namespace=pipecd --values secrets.yaml \
   --set-file config.data=values.yaml \
   --set-file secret.data.piped-key=pipekey
-  
