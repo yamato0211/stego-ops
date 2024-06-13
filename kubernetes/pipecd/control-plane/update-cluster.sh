7247f9c462d898b2821d0f1d7b63c6112c7bc84e
yamato0211 <kumayama0211@gmail.com>
2024-06-13 13:43:33 +0900

    feat(k8s): introduce pipecd

--- kubernetes/pipecd/control-plane/update-cluster.sh
+++ kubernetes/pipecd/control-plane/update-cluster.sh
@@ -1,5 +1,5 @@
 helm upgrade -i pipecd oci://ghcr.io/pipe-cd/chart/pipecd --version v0.47.2 --namespace=pipecd --create-namespace --values values.yaml \
-  --set-file config.data=secret.yaml \
+  --set-file config.data=secrets.yaml \
   --set-file secret.encryptionKey.data=encryption-key \
   --set-file secret.firestoreServiceAccount.data=firestore.json \
   --set-file secret.gcsServiceAccount.data=cloudstorage.json
